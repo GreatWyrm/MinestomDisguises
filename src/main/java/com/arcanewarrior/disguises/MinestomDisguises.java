c4de572eb19533a7e9788d0f9f876cf4fa784fc3
super <superjojominecraft@gmail.com>
2022-12-31 12:48:15 +0100

    Only API in Config

--- src/main/java/com/arcanewarrior/disguises/MinestomDisguises.java
+++ src/main/java/com/arcanewarrior/disguises/MinestomDisguises.java
@@ -2,6 +2,7 @@ package com.arcanewarrior.disguises;
 
 import com.arcanewarrior.disguises.commands.CommandInitializer;
 import com.arcanewarrior.disguises.events.EventInitializer;
+import com.moandjiezana.toml.Toml;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.tag.Tag;
 import xyz.citywide.citystom.Extension;
@@ -19,12 +20,18 @@ public final class MinestomDisguises extends Extension {
     public void initialize() {
         instance = this;
 
+        final Toml config = getConfig();
+
         eventInitializer = new EventInitializer(getEventNode(), hideTag);
         disguiseManager = new DisguiseManager(hideTag);
-        commandInitializer = new CommandInitializer(disguiseManager);
-
-        commandInitializer.registerAll(this);
         eventInitializer.registerAll();
+
+        if(config != null) {
+            if (!config.getBoolean("only-api", false)) {
+                commandInitializer = new CommandInitializer(disguiseManager);
+                commandInitializer.registerAll(this);
+            }
+        }
     }
 
     @Override
